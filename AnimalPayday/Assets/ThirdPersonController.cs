72a340efc091e501b78ca3953a9062e38c69a27d
Manuel Fritz <Manuel Fritz@gamer.dss.lan>
2016-04-15 22:04:05 +0200

    ADDED ThirdPersonController - move forward - rotate

---
+++ AnimalPayday/Assets/ThirdPersonController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonController : MonoBehaviour {
+
+	private Vector3 moveDirection = Vector3.zero;
+
+	[SerializeField]
+	float speed = 5f;
+
+	[SerializeField]
+	float rotationspeed = 3f;
+
+	float gravity = 9.81f;
+
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController> ();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3 (0, -1, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+
+			transform.Rotate(0,Input.GetAxis("Horizontal") * rotationspeed, 0);
+
+
+		}	
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+	
+	}
+		
+}
